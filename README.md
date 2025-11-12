9689ff9c4b4131163b3ce1fe2e39f114f441929c
SanderNF <sandernf@outlook.com>
2025-11-12 11:54:27 +0100

    working prototype v1

--- README.md
+++ README.md
@@ -6,9 +6,10 @@ i'm going to turn this into a web site using either python http server or flask
 it comunicates with a local palworld server hosted in a debian LXC container \
 this comunication hapens using the rest api on port `:8212`
 
-example file structure
+curent file structure
 ```
-\index.html
+\templates\index.html
 \httpServer.py
 \palworldApi.py
+\requirements.txt
 ```
--- palworldApi.py
+++ palworldApi.py
@@ -1,6 +1,93 @@
 import requests, json, math
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
 
 url = "http://192.168.86.128:8212/v1/api"
+
+
+@app.route('/')
+def indexHtml():
+    return render_template('index.html')
+
+@app.route('/announce', methods=['POST'])
+def announce():
+    msg = request.form['msg'] # 'navn' = name på inputen i HTML
+    headers = {
+        'Content-Type': 'application/json',
+        'authorization': 'Basic YWRtaW46Ynl0ZXMxMjM='
+    }
+    payload = json.dumps({
+      "message": msg
+    })
+    response = requests.request("POST", f'{url}/announce', headers=headers, data=payload)
+    return redirect('/')
+
+@app.route('/save', methods=['POST'])
+def save():
+    payload={}
+    headers = {
+        'Accept': 'application/json',
+        'authorization': 'Basic YWRtaW46Ynl0ZXMxMjM='
+    }
+    response = requests.request("POST", f'{url}/save', headers=headers, data=payload)
+    return redirect('/')
+
+@app.route('/metrics', methods=['GET'])
+def metrics():
+    payload={}
+    headers = {
+        'Accept': 'application/json',
+        'authorization': 'Basic YWRtaW46Ynl0ZXMxMjM='
+    }
+    response = requests.request("GET", f'{url}/metrics', headers=headers, data=payload)
+    res = json.loads(response.text)
+    #print(res)
+    uptime = res["uptime"]
+    seconds = uptime%60
+    minutes = math.floor((uptime)/60)%60
+    hours = math.floor((uptime)/3600)%24
+    days = math.floor(uptime/(60*60*24))
+    #print(seconds, minutes, hours, days)
+    #print(f'server uptime: {days}d {hours}h {minutes}m {seconds}s')
+    betterUpTime = f'server uptime: {days}d {hours}h {minutes}m {seconds}s'
+    return json.dumps({
+        "uptime": betterUpTime,
+        "players": res["currentplayernum"],
+        "maxPlayers": res["maxplayernum"],
+        "serverFps": res["serverfps"],
+        "serverFrameTime": res["serverframetime"],
+        "ingameDays": res["days"],
+    })
+    
+@app.route('/players', methods=['GET'])
+def players():
+    payload={}
+    headers = {
+        'Accept': 'application/json',
+        'authorization': 'Basic YWRtaW46Ynl0ZXMxMjM='
+    }
+    response = requests.request("GET", f'{url}/players', headers=headers, data=payload)
+    #print(response.text)
+    print(response.status_code)
+    basicPlayers = []
+    for i in json.loads(response.text)["players"]:
+        temp = {
+            "name": i["name"],
+            "accountName": i["accountName"],
+            "ping": i["ping"],
+            "level": i["level"],
+            "x": i["location_x"],
+            "y": i["location_y"],
+        }
+        basicPlayers.append(temp)
+    return json.dumps(basicPlayers)
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8213, debug=True)
+
 
 payload={}
 headers = {
@@ -25,7 +112,7 @@ uptime = res["uptime"]
 seconds = uptime%60
 minutes = math.floor((uptime)/60)%60
 hours = math.floor((uptime)/3600)%24
-days = math.floor(uptime/216000)
+days = math.floor(uptime/(60*60*24))
 print(seconds, minutes, hours, days)
 print(f'server uptime: {days}d {hours}h {minutes}m {seconds}s')
 print(response.status_code)
